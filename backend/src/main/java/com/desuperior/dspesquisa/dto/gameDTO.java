cfe7b4caaf5a5b820f5b26f05834d4fc6f47805e
viniciuspod <viniciuspodantas@gmail.com>
2020-09-16 21:23:17 -0300

    Ready for production

--- backend/src/main/java/com/desuperior/dspesquisa/dto/gameDTO.java
+++ backend/src/main/java/com/desuperior/dspesquisa/dto/gameDTO.java
@@ -30,6 +30,35 @@ public class gameDTO implements Serializable {
 		this.title = title;
 		this.platform = platform;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 	
 	
 }
